15ba64699b72225653e90c49f6af25e8bf4f483b
Rishikeshk9 <rushikeshkardile9@gmail.com>
2021-04-30 23:50:02 +0530

    Added Investors section

--- scripts/script.js
+++ scripts/script.js
@@ -97,3 +97,19 @@ $(window).on("load", function () {
   $(".loader-wrapper").fadeOut("slow");
 });
 AOS.init();
+
+
+const inViewport = (entries, observer) => {
+  entries.forEach(entry => {
+    entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+  });
+};
+
+const Obs = new IntersectionObserver(inViewport);
+const obsOptions = {}; //See: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options
+
+// Attach observer to every [data-inviewport] element:
+const ELs_inViewport = document.querySelectorAll('[data-inviewport]');
+ELs_inViewport.forEach(EL => {
+  Obs.observe(EL, obsOptions);
+});
